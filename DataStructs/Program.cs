4cee52f0830cfe530c79e3ff1841194bf18216c9
SamirKurbatov <samir.kurbatov.03@inbox.ru>
2023-04-22 20:18:15 +0300

    Добавьте файлы проекта.

---
+++ DataStructs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Tracing;
+
+
+#region LinkedList
+//var list = new LinkedList<int>();
+
+
+//list.Add(2);
+//list.Add(3);
+//list.Add(4);
+
+//list.InsertAfter(4, 66);
+
+
+//foreach (var item in list)
+//{
+//    Console.Write(item + " ");
+//}
+
+//Console.ReadLine();
+#endregion
+
+
+

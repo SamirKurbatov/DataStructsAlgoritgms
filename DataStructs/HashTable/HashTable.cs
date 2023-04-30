89abbe8208c26fc9584c52f4a1a61e1a181d2779
SamirKurbatov <samir.kurbatov.03@inbox.ru>
2023-04-30 18:42:54 +0300

    Сделал HashTable

---
+++ DataStructs/HashTable/HashTable.cs
@@ -0,0 +1,39 @@
+﻿namespace DataStructs.HashTable
+{
+    public class HashTable<T>
+    {
+        private Item<T>[] items;
+
+        public HashTable(int size)
+        {
+            items = new Item<T>[size];
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = new Item<T>(i);
+            }
+        }
+
+        public void Add(T item)
+        {
+            var key = GetHashCode(item);
+            items[key].Nodes.Add(item);
+        }
+
+        public bool Search(T item)
+        {
+            var key = GetHashCode(item);
+            return items[key].Nodes.Contains(item);
+        }
+
+        private int GetHashCode(T item)
+        {
+            return item.GetHashCode() % items.Length;
+        }
+    }
+
+    public record Item<T>(int Key, List<T> Nodes)
+    {
+        public Item(int key) : this(key, new List<T>()) { }
+    }
+}
